58baf7da83c748c849e03c085a83b967707292c2
lihaifeng <helloworld_glb@qq.com>
2020-12-18 23:45:40 +0800

    更新一个更好的SPI程序

---
+++ 26/SPI_实现基本功能/Debug/obj/main.d
@@ -0,0 +1,79 @@
+obj/main.o: srcc/main.c \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\07_NosPrg/includes.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/user.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/printf.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/uart.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU/mcu.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/MKL36Z4.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cm0plus.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmInstr.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmFunc.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/system_MKL36Z4.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/system_MKL36Z4.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/cpu.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cm0plus.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmFunc.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmInstr.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/suart.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\04_GEC/gec.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/printf.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/flash.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/timer.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/incapture.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/pwm.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/wdog.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/lcd.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/gpio.h \
+ D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/spi.h
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\07_NosPrg/includes.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/user.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/printf.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/uart.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU/mcu.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/MKL36Z4.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cm0plus.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmInstr.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmFunc.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/system_MKL36Z4.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\startup/system_MKL36Z4.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/cpu.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cm0plus.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmFunc.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\02_CPU/core_cmInstr.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/suart.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\04_GEC/gec.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/printf.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/flash.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/timer.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/incapture.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/pwm.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/wdog.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\05_UserBoard/lcd.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/gpio.h:
+
+D:\Users\lihaifeng\Desktop\学习文件夹\嵌入式\实验考核题目\26\SPI_实现基本功能\03_MCU\MCU_drivers/spi.h:
